62de030465b13029b4ee169f4382de4ad5d3697a
Øystein <oysteinha@gmail.com>
2018-01-22 17:50:10 +0100

    Lager intro skjerm

--- Controller.py
+++ Controller.py
@@ -25,12 +25,15 @@ class Controller():
                 self.main_model.background.set_scaled_image(pygame.image.load('./card_blue.png'))
                 #pos = pygame.mouse.get_pos()
                 for model in self.main_model.getAllModels():
+
                     if(model.checkCollissionOnPoint(pos)):
+
                         #If you hit the blue card:
                         if(model.color == 1):
                             self.new_score = self.main_model.score + 1
                             self.main_model.setScore(self.new_score)
                             self.main_model.setCards(self.cardFactory.generate4Cards())
+
                         if(model.color == 0 and self.main_model.score >= 5):
                             self.new_score = self.main_model.score - 5
                             self.main_model.setScore(self.new_score)
